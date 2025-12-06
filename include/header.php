ecd1595cf4c71f2f067a5bc0d658ae87a655ade7
stephanefab <kouadiostephanefabienbrou@gmail.com>
2025-12-06 14:58:22 +0000

    ajustement de la page et des liens de navigation

--- include/header.php
+++ include/header.php
@@ -3,9 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <title><?= (empty($pageTitle)) ? $pageTitle : 'Sports Comments' ?></title>
+    <title><?= (!empty($pageTitle)) ? htmlspecialchars($pageTitle) : 'Sports Comments' ?></title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
 <body>
-    <div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/index.php">SportsComments</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/index.php">Home</a>
+        </li>
+
+        <?php if(isLogged()): ?>
+            <?php if(getCurrentUser()['role'] === 'admin'): ?>
+            <li class="nav-item">
+              <a class="nav-link" href="/admin">Dashboard</a>
+            </li>
+            <?php endif; ?>
+
+            <li class="nav-item">
+              <a class="nav-link" href="/me">Profil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-danger" href="/logout.php">Logout</a>
+            </li>
+        <?php else: ?>
+            <li class="nav-item">
+              <a class="nav-link" href="/login.php">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/register.php">Register</a>
+            </li>
+        <?php endif; ?>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<main class="container my-4">
--- me/index.php
+++ me/index.php
@@ -1,6 +1,9 @@
 <?php
     require_once __DIR__ . '/../functions.php';
-    isUser();
+    if(!isLogged()){
+        redirectToLogin();
+    }
+    $user = getCurrentUser();
 ?>
 
 <?php include_once __DIR__ . '/include/header.php'; ?>
